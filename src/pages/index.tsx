6ca0def208b626834bfecb84b2c471c5ce408d2d
Dennis <dennisjr89@gmail.com>
2023-03-11 17:34:04 -0500

    change favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,9 @@ const Home: NextPage = () => {
         <title>Modal Lesson</title>
         <meta name="description" content="Create lesson plans for teachers!" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </Head>
       <main>
         <h1>Welcome to Modal Lesson</h1>
