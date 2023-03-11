6ffd76554763d88e561374694e723f3820104b7a
Dennis <dennisjr89@gmail.com>
2023-03-10 19:36:39 -0500

    feat: add next auth config for google

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import { type NextPage } from "next";
+import { signIn, useSession } from "next-auth/react";
 import Head from "next/head";
 
 
 const Home: NextPage = () => {
+  const {data: session} = useSession(); 
+  console.log({session})
   return (
     <>
       <Head>
@@ -12,6 +15,7 @@ const Home: NextPage = () => {
       </Head>
       <main>
         <h1>Welcome to Modal Lesson</h1>
+        <button onClick={() => void signIn()}>asdf</button>
       </main>
     </>
   );
