2e79022086b8dbf172997667bab91269a6f82960
Dennis <dennisjr89@gmail.com>
2023-03-15 22:57:09 -0400

    config for tiptap

--- src/components/Tiptap.tsx
+++ src/components/Tiptap.tsx
@@ -5,9 +5,31 @@ const Tiptap = () => {
   const editor = useEditor({
     extensions: [StarterKit],
     content: "<p>Hello World! 🌎️</p>",
+    editorProps: {
+      attributes: {
+        class:
+          "prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none",
+      },
+    },
   });
 
-  return <EditorContent editor={editor} />;
+  // editor?.chain().focus().toggleBold().run();
+
+  return (
+    <>
+      <button onClick={() => editor?.chain().focus().toggleBold().run()}>
+        bold
+      </button>
+      <button
+        onClick={() =>
+          editor?.chain().focus().toggleHeading({ level: 2 }).run()
+        }
+      >
+        H1
+      </button>
+      <EditorContent editor={editor} />
+    </>
+  );
 };
 
 export default Tiptap;
